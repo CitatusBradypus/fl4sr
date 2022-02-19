286824f39500e52ea6e1d2abdffec69de16b1e84
CitatusBradypus <pikmanjan@gmail.com>
2022-02-19 12:48:46 +0100

    Actor layer normalisation

--- src/models.py
+++ src/models.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3.8
 import sys
 import os
+from matplotlib.pyplot import step
 HOME = os.environ['HOME']
 sys.path.append(HOME + '/catkin_ws/src/fl4sr/src')
 import torch
@@ -32,15 +33,19 @@ class Actor(nn.Module):
         self.layers = nn.ModuleList()
         for i in range(self.hidden_dimension):
             if i == 0:
+                self.layers.append(nn.LayerNorm(input_dimension))
                 self.layers.append(nn.Linear(input_dimension, hidden_layers[i]))
+                self.layers.append(nn.LayerNorm(hidden_layers[i]))                
                 self.layers.append(nn.Linear(hidden_layers[i], hidden_layers[i+1]))
             elif i == self.hidden_dimension - 1:
+                self.layers.append(nn.LayerNorm(hidden_layers[i]))
                 self.layers.append(nn.Linear(hidden_layers[i], 1))
                 nn.init.xavier_normal_(self.layers[-1].weight)
                 self.layers.append(nn.Linear(hidden_layers[i], 1))
                 nn.init.xavier_normal_(self.layers[-1].weight)
             else:
-               self.layers.append(nn.Linear(hidden_layers[i], hidden_layers[i+1]))
+                self.layers.append(nn.LayerNorm(hidden_layers[i]))
+                self.layers.append(nn.Linear(hidden_layers[i], hidden_layers[i+1]))
         self.layers_len = len(self.layers)
         # prepare activation functions
         self.ReLU = nn.ReLU()
@@ -60,12 +65,14 @@ class Actor(nn.Module):
             torch.tensor: 2d output tensor.
         """
         # run computation
-        for i in range(self.layers_len - 1):
-            if i == self.layers_len - 2:
-                output0 = self.tanh(self.layers[i](x))
-                output1 = self.sigmoid(self.layers[i + 1](x))
+        for i in range(0, self.layers_len - 1, 2):
+            if i == self.layers_len - 3:
+                x = self.layers[i](x)
+                output0 = self.tanh(self.layers[i + 1](x))
+                output1 = self.sigmoid(self.layers[i + 2](x))
             else:
-                x = self.ReLU(self.layers[i](x))
+                x = self.layers[i](x)
+                x = self.ReLU(self.layers[i + 1](x))
         return torch.cat((output0, output1), dim=1)
 
 
@@ -127,14 +134,14 @@ class Critic(nn.Module):
 if __name__ == '__main__':
     actor = Actor(3, [512, 512, 512])
     actor.cuda()
-    input_t = torch.tensor([[0.5, 1000, 42.87]]).type(torch.cuda.FloatTensor)
+    input_t = torch.tensor([[-0.5, 0.1, -1]]).type(torch.cuda.FloatTensor)
     print(input_t.size())
     action = actor.forward(input_t)
     print(action.size())
     print(action)
 
-    critic = Critic(3, [512, 512, 512])
-    critic.cuda()
-    value = critic.forward(input_t)
-    print(value.size())
-    print(value)
+    #critic = Critic(3, [512, 512, 512])
+    #critic.cuda()
+    #value = critic.forward(input_t)
+    #print(value.size())
+    #print(value)
