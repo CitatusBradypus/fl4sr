3caa756fb2c28042f23decbdbf73c97cbce4ee35
Miyembe <respct11@gmail.com>
2023-03-17 13:21:03 +0100

    small modification for user change

--- install_required_ros_packages.bash
+++ install_required_ros_packages.bash
@@ -1,4 +1,5 @@
 #/usr/bin/bash
+# Wrttien by Seongin Na
 # 1. Move to the parent dir
 # 2. Download dependency packages (turtlebot3, gazebo_ros_pkgs for StepControl)
 # 3. export TURTLEBOT3_MODEL env variable into .bashrc
